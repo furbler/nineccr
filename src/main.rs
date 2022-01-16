1f9785b9f1ca34249b83bd782e15b6ea24c8c74e
furbler <63544661+furbler@users.noreply.github.com>
2022-01-16 19:33:48 +0900

    引数の値を出力に反映する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let print_num: &str = match args.len() {
+        1 => "0",      //引数無し
+        _ => &args[1], //最初の引数の値
+    };
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", print_num);
+    println!("  ret");
 }
